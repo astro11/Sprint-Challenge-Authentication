91ba87480f7a6cd342ebbe280b455d52c6bfaad3
astro11 <aquila.strother@asu.edu>
2018-04-20 12:00:27 -0400

    Created User Functionality

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -5,6 +5,11 @@ const createUser = (req, res) => {
   const { username, password } = req.body;
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+const user = new User({ username, password });
+user
+.save().then(insertedUser => {
+  res.status(201).json({ insertedUser });
+  });
 };
 
 module.exports = {
