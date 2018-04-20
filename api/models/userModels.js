68375aa96e04e08de5ca42fa18b77a199551bf51
astro11 <aquila.strother@asu.edu>
2018-04-20 11:41:49 -0400

    added UserSchema - PreSave hook - checkPasswor d

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -4,10 +4,17 @@ const Schema = mongoose.Schema;
 
 const SALT_ROUNDS = 11;
 
-const UserSchema = Schema({
-  // create your user schema here.
-  // username: required, unique and lowercase
-  // password: required
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 UserSchema.pre('save', function(next) {
@@ -15,6 +22,10 @@ UserSchema.pre('save', function(next) {
   // Fill this middleware in with the Proper password encrypting, bcrypt.hash()
   // if there is an error here you'll need to handle it by calling next(err);
   // Once the password is encrypted, call next() so that your userController and create a user
+bcrypt.hash(this.password, SALT_ROUNDS).then(hash => {
+  this.password = hash;
+  next();
+  });
 });
 
 UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
@@ -22,6 +33,13 @@ UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
   // Fill this method in with the Proper password comparing, bcrypt.compare()
   // Your controller will be responsible for sending the information here for password comparison
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
+  bcrypt.compare(plainTextPW, this.password, function(err, isValid) {
+    if (err) {
+      return callBack(err);
+    }
+    //if no error
+    callBack(null, isValid);
+  });
 };
 
 module.exports = mongoose.model('User', UserSchema);
